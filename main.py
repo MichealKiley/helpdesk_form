01ce8e8cef7be47919a095b335d30817613a5c30
MichealKiley <157815731+MichealKiley@users.noreply.github.com>
2024-01-26 09:36:05 -0600

    Host

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Website import create_app
 
 app = create_app()
 
+
 if __name__ == "__main__":
     app.config["SECRET_KEY"] = "kjgbrekj;rbv;uiber;kberb"
-    app.run(host="10.1.5.231", port="9999", debug=True)
+    app.run(debug=True)
+
